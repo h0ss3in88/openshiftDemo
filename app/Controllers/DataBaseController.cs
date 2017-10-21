5a536f0f4627e2e7b4e42b5b934feab34f14b2f4
h0ss3in88 <mohammad.hussein.taherian@gmail.com>
2017-10-21 23:39:02 +0330

    add chinook database initializer

---
+++ app/Controllers/DataBaseController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace app.Controllers
+{
+  public class DataBaseController : Controller
+  {
+    private IHostingEnvironment _environment;
+    public DataBaseController(IHostingEnvironment environment)
+    {
+      _environment = environment;
+    }
+    // GET
+    public IActionResult Index()
+    {
+      return View();
+    }
+    // GET
+    public IActionResult CreateDataBase()
+    {
+      var connectionString = "server=postgresql;port=5432;user id=Hussein;password=123456;";
+      NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+      var sql = System.IO.File.ReadAllText(Path.Combine(_environment.ContentRootPath, "Files","pg_chinook_mod_to_lower.sql"), Encoding.UTF8);
+      var command = new NpgsqlCommand
+      {
+        Connection = connection,
+        CommandText = sql
+      };
+      connection.Open();
+      try
+      {
+        command.ExecuteNonQuery();
+        return Ok();
+      }
+      catch (NpgsqlException e)
+      {
+        return BadRequest(e.Message);
+        throw e;
+      }
+      finally
+      {
+        connection.Close();
+      }
+    }
+  }
+}
